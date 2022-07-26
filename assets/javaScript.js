3a0b3ae6272cfcf135a3fdb0f76ec2873f04226f
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-26 13:21:43 +0000

    initial commit

---
+++ assets/javaScript.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class DrumMaschine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state({
+
+        });
+    }
+}
+
+
+ReactDOM.render(<div><p>BLABLS</p></div>, document.getElementById("root"));
