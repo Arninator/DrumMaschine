e8977eb246239370c32fb47f6883aa2558e08e0b
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-26 15:20:51 +0000

    import links build the base structure of the files

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/*console.log("HIER");
 
 class DrumMaschine extends React.Component {
     constructor(props) {
@@ -7,8 +6,31 @@ class DrumMaschine extends React.Component {
         this.state({
 
         });
+        
+    }
+    render() {
+        return(
+            <div id="drum-maschine">
+                <button class="drum-pad" id="q-pad">Q</button>
+                <button class="drum-pad" id="w-pad">W</button>
+                <button class="drum-pad" id="e-pad">E</button>
+                <button class="drum-pad" id="a-pad">A</button>
+                <button class="drum-pad" id="s-pad">S</button>
+                <button class="drum-pad" id="d-pad">D</button>
+                <button class="drum-pad" id="y-pad">Y</button>
+                <button class="drum-pad" id="x-pad">X</button>
+                <button class="drum-pad" id="c-pad">C</button>
+            </div>
+        );
     }
 }
 
+ReactDOM.render(<DrumMaschine />, document.getElementById("root"));*/
 
-ReactDOM.render(<div><p>BLABLS</p></div>, document.getElementById("root"));
+/*const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render();*/
+
+const myelement = <h1>Hello World</h1>;
+
+ReactDOM.render(myelement, document.getElementById('root'));
