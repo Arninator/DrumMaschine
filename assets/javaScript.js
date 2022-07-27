b0df7ff6dbd0c030027f2dc4cb4f41bfc09ffc84
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-27 12:51:14 +0000

    style the drum maschine in grey tones

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -6,17 +6,31 @@ class DrumMaschine extends React.Component {
     }
     render() {
         return(
-            <div id="drum-maschine">
-                <button type="button" class="drum-pad btn" id="q-pad">Q</button>
-                <button type="button" class="drum-pad" id="w-pad">W</button>
-                <button type="button" class="drum-pad" id="e-pad">E</button>
-                <button type="button" class="drum-pad" id="a-pad">A</button>
-                <button type="button" class="drum-pad" id="s-pad">S</button>
-                <button type="button" class="drum-pad" id="d-pad">D</button>
-                <button type="button" class="drum-pad" id="y-pad">Y</button>
-                <button type="button" class="drum-pad" id="x-pad">X</button>
-                <button type="button" class="drum-pad" id="c-pad">C</button>
+            <div id="drum-container">
+                <div class="drum-maschine" id="drum-maschine">
+                    <button type="button" class="drum-pad btn" id="q-pad">Q</button>
+                    <button type="button" class="drum-pad btn" id="w-pad">W</button>
+                    <button type="button" class="drum-pad btn" id="e-pad">E</button>
+                    <button type="button" class="drum-pad btn" id="a-pad">A</button>
+                    <button type="button" class="drum-pad btn" id="s-pad">S</button>
+                    <button type="button" class="drum-pad btn" id="d-pad">D</button>
+                    <button type="button" class="drum-pad btn" id="y-pad">Y</button>
+                    <button type="button" class="drum-pad btn" id="x-pad">X</button>
+                    <button type="button" class="drum-pad btn" id="c-pad">C</button>
+                </div>
+                <div class="drum-maschine" id="drum-maschine2">
+                    <button type="button" class="drum-pad btn" id="7-pad">7</button>
+                    <button type="button" class="drum-pad btn" id="8-pad">8</button>
+                    <button type="button" class="drum-pad btn" id="9-pad">9</button>
+                    <button type="button" class="drum-pad btn" id="4-pad">4</button>
+                    <button type="button" class="drum-pad btn" id="5-pad">5</button>
+                    <button type="button" class="drum-pad btn" id="6-pad">6</button>
+                    <button type="button" class="drum-pad btn" id="1-pad">1</button>
+                    <button type="button" class="drum-pad btn" id="2-pad">2</button>
+                    <button type="button" class="drum-pad btn" id="3-pad">3</button>
+                </div>
             </div>
+            
         );
     }
 }
