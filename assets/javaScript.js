e47029489f88cd032fea531ebac06d582f20bd2e
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-27 11:45:21 +0000

    try to style the button

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -7,15 +7,15 @@ class DrumMaschine extends React.Component {
     render() {
         return(
             <div id="drum-maschine">
-                <button class="drum-pad button" id="q-pad">Q</button>
-                <button class="drum-pad" id="w-pad">W</button>
-                <button class="drum-pad" id="e-pad">E</button>
-                <button class="drum-pad" id="a-pad">A</button>
-                <button class="drum-pad" id="s-pad">S</button>
-                <button class="drum-pad" id="d-pad">D</button>
-                <button class="drum-pad" id="y-pad">Y</button>
-                <button class="drum-pad" id="x-pad">X</button>
-                <button class="drum-pad" id="c-pad">C</button>
+                <button type="button" class="drum-pad btn" id="q-pad">Q</button>
+                <button type="button" class="drum-pad" id="w-pad">W</button>
+                <button type="button" class="drum-pad" id="e-pad">E</button>
+                <button type="button" class="drum-pad" id="a-pad">A</button>
+                <button type="button" class="drum-pad" id="s-pad">S</button>
+                <button type="button" class="drum-pad" id="d-pad">D</button>
+                <button type="button" class="drum-pad" id="y-pad">Y</button>
+                <button type="button" class="drum-pad" id="x-pad">X</button>
+                <button type="button" class="drum-pad" id="c-pad">C</button>
             </div>
         );
     }
