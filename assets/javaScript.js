34c9515d49a116ed9e5bd0651bb81a4ea8010a8a
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-27 16:23:12 +0000

    load audio data into the file system and connect one to the first button

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -2,37 +2,62 @@
 class DrumMaschine extends React.Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+            id: "",
+            audioId: "",
+            char: "",
+            file: "",
+        }
+        this.handlePush = this.handlePush.bind(this);
+        this.playSound = this.playSound.bind(this);
+
+    }
+    handlePush (e) {
+        this.playSound(e.target.id);
+    }
+    playSound(id) {
+        const sound = document.getElementById("audio-" + id);
+        sound.currentTime = 0;
+        sound.play();
     }
     render() {
         return(
             <div id="drum-container">
-                <div class="drum-maschine" id="drum-maschine">
-                    <button type="button" class="drum-pad btn" id="q-pad">Q</button>
-                    <button type="button" class="drum-pad btn" id="w-pad">W</button>
-                    <button type="button" class="drum-pad btn" id="e-pad">E</button>
-                    <button type="button" class="drum-pad btn" id="a-pad">A</button>
-                    <button type="button" class="drum-pad btn" id="s-pad">S</button>
-                    <button type="button" class="drum-pad btn" id="d-pad">D</button>
-                    <button type="button" class="drum-pad btn" id="y-pad">Y</button>
-                    <button type="button" class="drum-pad btn" id="x-pad">X</button>
-                    <button type="button" class="drum-pad btn" id="c-pad">C</button>
+                <div className="drum-maschine" id="drum-maschine" onClick={this.handlePush}>
+                    <Button id="Q-pad" char="Q" file="audio/CUT-KIK(.WAV"/>
+                    <button type="button" className="drum-pad btn" id="w-pad">W</button>
+                    <button type="button" className="drum-pad btn" id="e-pad">E</button>
+                    <button type="button" className="drum-pad btn" id="a-pad">A</button>
+                    <button type="button" className="drum-pad btn" id="s-pad">S</button>
+                    <button type="button" className="drum-pad btn" id="d-pad">D</button>
+                    <button type="button" className="drum-pad btn" id="y-pad">Y</button>
+                    <button type="button" className="drum-pad btn" id="x-pad">X</button>
+                    <button type="button" className="drum-pad btn" id="c-pad">C</button>
                 </div>
-                <div class="drum-maschine" id="drum-maschine2">
-                    <button type="button" class="drum-pad btn" id="7-pad">7</button>
-                    <button type="button" class="drum-pad btn" id="8-pad">8</button>
-                    <button type="button" class="drum-pad btn" id="9-pad">9</button>
-                    <button type="button" class="drum-pad btn" id="4-pad">4</button>
-                    <button type="button" class="drum-pad btn" id="5-pad">5</button>
-                    <button type="button" class="drum-pad btn" id="6-pad">6</button>
-                    <button type="button" class="drum-pad btn" id="1-pad">1</button>
-                    <button type="button" class="drum-pad btn" id="2-pad">2</button>
-                    <button type="button" class="drum-pad btn" id="3-pad">3</button>
+                <div className="drum-maschine" id="drum-maschine2">
+                    <button type="button" className="drum-pad btn" id="7-pad">7</button>
+                    <button type="button" className="drum-pad btn" id="8-pad">8</button>
+                    <button type="button" className="drum-pad btn" id="9-pad">9</button>
+                    <button type="button" className="drum-pad btn" id="4-pad">4</button>
+                    <button type="button" className="drum-pad btn" id="5-pad">5</button>
+                    <button type="button" className="drum-pad btn" id="6-pad">6</button>
+                    <button type="button" className="drum-pad btn" id="1-pad">1</button>
+                    <button type="button" className="drum-pad btn" id="2-pad">2</button>
+                    <button type="button" className="drum-pad btn" id="3-pad">3</button>
                 </div>
             </div>
             
         );
     }
+}
+const Button = (props) => {
+    // console.log("--- " + props.id)
+    return(
+        <div>
+            <button type="button" className="drum-pad btn" id={props.id} char={props.char} file={props.file} onClick={props.onClick}>{props.char}</button>
+            <audio id={"audio-" + props.id} ><source src={props.file} type="audio/mpeg" /></audio>
+        </div>
+    );
 }
 
 ReactDOM.render(<DrumMaschine />, document.getElementById("root"));
