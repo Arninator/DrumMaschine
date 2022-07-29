26e9988ee419bfe8db0197eb5de6a25fc1758133
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-29 15:23:11 +0000

    style the button like an modern drum kit

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,23 +1,3 @@
-const keyCodes = {
-    81: "Q",
-    87: "W",
-    69: "E",
-    65: "A",
-    83: "S",
-    68: "D",
-    89: "Y",
-    88: "X",
-    67: "C",
-    103: "7",
-    104: "8",
-    105: "9",
-    100: "4",
-    101: "5",
-    102: "6",
-    97: "1",
-    98: "2",
-    99: "3"
-}
 
 class DrumMaschine extends React.Component {
     constructor(props) {
@@ -46,16 +26,20 @@ class DrumMaschine extends React.Component {
     }
     pressKey(e) {
         const code = e.code.charAt(3);
-        // console.log(code);
         this.playSound(code + "-pad");
     }
     playSound(id) {
         const sound = document.getElementById("audio-" + id);
+        const button = document.getElementById(id);
+        
         sound.currentTime = 0;
         sound.play();
+
+        button.classList.add("drum-pad:active");
+        button.to();
     }
 
-    
+
     render() {
         return(
             <div id="drum-container">
@@ -66,20 +50,20 @@ class DrumMaschine extends React.Component {
                     <Button id="A-pad" char="A" file="audio/CL-HH1(L.WAV" />
                     <Button id="S-pad" char="S" file="audio/FILTKIK(.WAV" />
                     <Button id="D-pad" char="D" file="audio/HORNZ(LD.WAV" />
-                    <Button id="Y-pad" char="Y" file="audio/KIZICK(L.WAV" />
+                    <Button id="Z-pad" char="Z" file="audio/KIZICK(L.WAV" />
                     <Button id="X-pad" char="X" file="audio/RATTLEKI.WAV" />
                     <Button id="C-pad" char="C" file="audio/SLICESNR.WAV" />
                 </div>
-                <div className="drum-maschine" id="drum-maschine2">
-                    <button type="button" className="drum-pad btn" id="7-pad">7</button>
-                    <button type="button" className="drum-pad btn" id="8-pad">8</button>
-                    <button type="button" className="drum-pad btn" id="9-pad">9</button>
-                    <button type="button" className="drum-pad btn" id="4-pad">4</button>
-                    <button type="button" className="drum-pad btn" id="5-pad">5</button>
-                    <button type="button" className="drum-pad btn" id="6-pad">6</button>
-                    <button type="button" className="drum-pad btn" id="1-pad">1</button>
-                    <button type="button" className="drum-pad btn" id="2-pad">2</button>
-                    <button type="button" className="drum-pad btn" id="3-pad">3</button>
+                <div className="drum-maschine" id="drum-maschine2" onClick={this.handlePush} onKeyDown={this.pressKey}>
+                    <Button id="Y-pad" char="Y" file="audio/CUT-KIK(.WAV" />
+                    <Button id="U-pad" char="U" file="audio/BASSD.WAV" />
+                    <Button id="I-pad" char="I" file="audio/AIR-SNR(.WAV" />
+                    <Button id="H-pad" char="H" file="audio/CL-HH1(L.WAV" />
+                    <Button id="J-pad" char="J" file="audio/FILTKIK(.WAV" />
+                    <Button id="K-pad" char="K" file="audio/HORNZ(LD.WAV" />
+                    <Button id="B-pad" char="B" file="audio/KIZICK(L.WAV" />
+                    <Button id="N-pad" char="N" file="audio/RATTLEKI.WAV" />
+                    <Button id="M-pad" char="M" file="audio/SLICESNR.WAV" />
                 </div>
             </div>
             
@@ -91,7 +75,8 @@ const Button = (props) => {
     return(
         <button
             className="drum-pad"
-            id={props.id} char={props.char}
+            id={props.id} 
+            char={props.char}
             file={props.file}
             onClick={props.onClick}
             onKeyDown={props.onKeyDown}>
